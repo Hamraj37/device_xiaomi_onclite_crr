22c7e899fb1f2e9a0bf8385e803a80333de1bc2e
Hamraj37 <skhamraj37@gmail.com>
2024-03-27 16:49:25 +0530

    afterlife

--- afterlife_onclite.mk
+++ afterlife_onclite.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common afterlife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := afterlife_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
@@ -28,6 +28,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Common Stuff
+AFTERLIFE_MAINTAINER := LizRelDev
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+
+# Allow usage of custom binary linker (LD), default is false
+TARGET_KERNEL_OPTIONAL_LD := true
+
+# AudioFx
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Fix uses broken libraries
+RELAX_USES_LIBRARY_CHECK := true
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 # Define bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 720
